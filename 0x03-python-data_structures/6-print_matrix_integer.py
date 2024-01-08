602fe14914e8d8f430c209c7a4221ec0cec8b7c3
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-01-08 12:39:04 -0800

    pep8 fulfilled

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     """Print a matrix of integers."""
     for m in range(len(matrix)):
         for n in range(len(matrix[m])):
-                print("{:d}".format(matrix[m][n]), end="")
-                if n != (len(matrix[m]) - 1):
-                    print(" ", end="")
-        print("")
+            print("{:d}".format(matrix[m][n]), end="")
+            if n != (len(matrix[m]) - 1):
+                print(" ", end="")
+            print("")
