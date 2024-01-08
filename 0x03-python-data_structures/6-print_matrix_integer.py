1546464a87426ba4bf81ee3fd06ea49868ed6f68
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-01-08 12:36:41 -0800

    print elements of a matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """Print a matrix of integers."""
+    for m in range(len(matrix)):
+        for n in range(len(matrix[m])):
+                print("{:d}".format(matrix[m][n]), end="")
+                if n != (len(matrix[m]) - 1):
+                    print(" ", end="")
+        print("")
