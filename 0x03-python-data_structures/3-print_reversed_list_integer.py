b3cec743c826671bbd2b6ea663424a17839df185
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-01-08 18:08:50 -0800

    reverse list uding built-in methods

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
-def print_reversed_list_integer(my_list=[]):
-    idx = len(my_list) - 1
-
-    while (idx >= 0):
-        print("{:d}".format(my_list[idx]))
-        idx -= 1
+def print_reversed_list_integer(my_list=None):
+    """Print all integers of a list in reverse order."""
+    if my_list is None:
+        my_list = []
+    if not isinstance(my_list, list):
+        return
+    
+    reversed_list = list(my_list)
+    reversed_list.reverse()
+    for i in reversed_list:
+        print("{:d}".format(i))
