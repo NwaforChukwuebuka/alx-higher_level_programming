c272e0824aefbaf816cb52eb245a206fe5086bac
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-01-10 20:59:44 -0800

    ff

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,5 @@
 
 def print_sorted_dictionary(a_dictionary):
     """Print a dictionary by ordered keys."""
-    [print("{}: {}".format(key, a_dictionary[key])) for key in sorted(a_dictionary)]
+    [print("{}: {}".format(key, a_dictionary[key]))
+        for key in sorted(a_dictionary)]
