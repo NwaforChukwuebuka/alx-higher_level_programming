f4c17036983ce24ae24beb6d3b5c4bc700ed815b
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-01-10 20:51:22 -0800

    pep8 line indentation compliant

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -24,7 +24,7 @@ def roman_to_int(roman_string):
 
         if (x != (len(roman_string) - 1) and
                 roman_dict[roman_string[x]] < roman_dict[roman_string[x + 1]]):
-                num_int += roman_dict[roman_string[x]] * -1
+            num_int += roman_dict[roman_string[x]] * -1
 
         else:
             num_int += roman_dict[roman_string[x]]
