479468bb1f8598b136e2c0abf4c6795cb2e79803
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-01-22 14:59:12 -0800

    Safe printing of list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    
+    num_of_ele_printed = 0
+
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            num_of_ele_printed += 1
+
+    except IndexError as IE:
+        pass
+
+    print("")
+    return (num_of_ele_printed)
