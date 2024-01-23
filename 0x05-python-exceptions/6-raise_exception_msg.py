4346220703daaff5db8c2bbc62b44887a009c01e
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-01-22 17:24:47 -0800

    Raising exceptions with a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Nwafor Chukwuebuka <e.nwafor13@gmail.com>
+# 6-raise_exception_msg.py
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
