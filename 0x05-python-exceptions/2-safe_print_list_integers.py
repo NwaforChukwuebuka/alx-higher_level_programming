4b538053a815c335cc4a5c618d3e8d8de21621d0
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-01-22 16:21:38 -0800

    used continue instead of pass

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,21 @@
-#!/usr//bin/python3
+#!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-   
+    """Print the first x elements of a list that are integers.
+
+    Args:
+        my_list (list): Given list
+        x (int): Total integer to print from list
+
+    Returns:
+        The number of integers printed.
+    """
     int_printed = 0
-
-    for i in range(0, x):
+    for index in range(0, x):
         try:
-                print("{:d}".format(my_list[i]), end="")
-                int_printed += 1
-
-        except (TypeError, ValueError):
-            pass
-
-    print()
+            print("{:d}".format(my_list[index]), end="")
+            int_printed += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
     return (int_printed)
