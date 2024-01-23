5fdfa9dde1a78694b0f663827b2f9e90292f66f8
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-01-22 16:14:04 -0800

    Dont catch IndexError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,18 +1,16 @@
-#!/usr/bin/python3
+#!/usr//bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-
+   
     int_printed = 0
-    try:
-        for i in range(x):
-            try:
-                print(f"{my_list[i]:d}", end="")
-                int_printed += 1
-            except (TypeError, ValueError):
-                pass
 
-    except IndexError:
-        pass
+    for i in range(0, x):
+        try:
+                print("{:d}".format(my_list[i]), end="")
+                int_printed += 1
+
+        except (TypeError, ValueError):
+            pass
 
     print()
     return (int_printed)
