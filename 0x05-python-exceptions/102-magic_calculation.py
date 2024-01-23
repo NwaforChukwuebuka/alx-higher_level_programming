cc89cca1bc04744e7aee8ea40c1b568ca659fb4d
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-01-22 18:18:02 -0800

    Bytecode to python code

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+
+    result = 0
+
+    for i in range(1, 4):
+
+        try:
+            if i > a:
+                raise Exception('Too far')
+            result += pow(a, b) / i
+
+        except Exception:
+            result += a + b
+
+    return result
