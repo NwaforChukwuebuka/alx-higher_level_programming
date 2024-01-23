29ffa729d87ad151dad4a77379dfb080bdb90c93
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-01-22 17:59:24 -0800

    saving the erro in a variable

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
-# Nwafor Chukwuebuka <e.nwafor13@gmail.com>
-# 100-safe_print_integer_err.py
 
 import sys
 
 
 def safe_print_integer_err(value):
-    """Prints an integer with "{:d}".format().
 
-    Args:
-        value (int): The integer to print.
-
-    Return:
-        True if print succeeds
-        False if print fails
-    """
     try:
         print("{:d}".format(value))
         return (True)
 
-    except (ValueError, ValueError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+    except (ValueError, TypeError) as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return (False)
