7a0b6ad61a44fade038f9f8b45d45c5af5e32f9d
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-01-23 15:26:13 -0800

    Square clsss with private instance attr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" Square Class
+    Instance Attribute: size
+"""
+
+
+class Square:
+    """ Creates instance of shape square with private instance attr"""
+
+    def __init__(self, size) -> None:
+        self.__size = size
