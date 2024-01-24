d43e50f590835591bca5940f3e83b60d5a9d7255
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-01-23 16:09:19 -0800

    Returns the square of the shape

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""""
+A Square class with private instance attr 'size'
+               and  public instance 'area()'.
+
+"""
+
+
+class Square:
+    """ A Square class with private instance attr 'size'
+                        and  public instance 'area()'
+    
+    Attributes:
+        size (int) : The size of the instance. Default is zero
+
+    Raises:
+        TypeError: if the size is not an integer
+        ValueError: if the size is less than 0
+
+    Example:
+        >>> obj = MyClass(size=5)
+        >>> print(obj.size)
+        5
+
+    """
+
+    def __init__(self, size = 0) -> None:
+        """
+        Initialize the instance with a specified size.
+
+        Args:
+            size (int, optional): The size of the instance. Defaults to 0.
+                Must be an integer, and size >= 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Returns the area of the square"""
+        return (self.__size ** 2)
