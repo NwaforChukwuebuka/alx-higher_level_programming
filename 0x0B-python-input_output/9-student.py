9d88138dc5a9d067b210d85820d3d08d3e0cdef7
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-02-06 18:58:57 -0800

    represent a python class to json file

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# 99-student.py
+# Nwafor CHukwuebuka
+"""Defines a class Student."""
+
+
+class Student:
+    """Represent a student class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student object.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): Age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns a dictionary representation of the Student class"""
+        return self.__dict__
