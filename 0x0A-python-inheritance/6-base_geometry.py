dc5c83e15a24883eef05f9c3bca91e2cf9d7d99c
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-02-07 18:17:56 -0800

    Adds area method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+# Nwafor Chukwuebuka
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry empty class"""
+    pass
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
