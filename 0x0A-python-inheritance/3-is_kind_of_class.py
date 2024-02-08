c9e1aaa62529ffd0f81e128db518e1635162cc3e
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-02-07 17:55:35 -0800

    PEP8 compliant

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -10,9 +10,10 @@ def is_kind_of_class(obj, a_class):
     Args:
         obj: The object to check.
         a_class: The class to match the type of obj to.
-        
+
     Returns:
-        True if obj is an instance or inherited instance of a_class, otherwise False.
+        True if obj is an instance or
+        inherited instance of a_class, otherwise False.
     """
     if isinstance(obj, a_class):
         return True
