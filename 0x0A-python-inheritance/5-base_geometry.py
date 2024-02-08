c58a8228b9057759a928ffbf11427350936f0240
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-02-07 18:15:04 -0800

    Empty BaseGeometry class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Nwafor Chukwuebuka
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry empty class"""
+    pass
