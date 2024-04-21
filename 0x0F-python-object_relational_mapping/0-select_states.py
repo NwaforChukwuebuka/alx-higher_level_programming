d13d3d7da9410e9a825db8c655634feb43980f10
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-04-20 20:05:05 -0700

    Added docString

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Script to list all states from the database hbtn_0e_0_usa.
+"""
 # Lists all states from the database hbtn_0e_0_usa.
 # Usage: ./0-select_states.py <mysql username> \
 #                             <mysql password> \
