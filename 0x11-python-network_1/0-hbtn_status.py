26ecc5158982d5f82dcd5dbd7d6401bba0cb32d9
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-04-30 19:42:06 -0700

    Executable

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,14 @@
 # Fetches https://alx-intranet.hbtn.io/status
 import urllib.request as browser
 
-url = 'https://alx-intranet.hbtn.io/status'
-req = browser.Request(url)
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = browser.Request(url)
 
-with browser.urlopen(req) as response:
-    body = response.read()
+    with browser.urlopen(req) as response:
+        body = response.read()
 
-    print("Body response:")
-    print('\t- type: {}'.format(type(body)))
-    print('\t- content: {}'.format(body))
-    print('\t- utf8 content: {}'.format(body.decode("utf-8")))
+        print("Body response:")
+        print('\t- type: {}'.format(type(body)))
+        print('\t- content: {}'.format(body))
+        print('\t- utf8 content: {}'.format(body.decode("utf-8")))
