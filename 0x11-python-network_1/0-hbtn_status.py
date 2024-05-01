02f03dc73afa849e1374cf756aa3c7968bf6cd05
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-04-30 19:38:33 -0700

    Fetch a url and display attr

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Fetches https://alx-intranet.hbtn.io/status
+import urllib.request as browser
+
+url = 'https://alx-intranet.hbtn.io/status'
+req = browser.Request(url)
+
+with browser.urlopen(req) as response:
+    body = response.read()
+
+    print("Body response:")
+    print('\t- type: {}'.format(type(body)))
+    print('\t- content: {}'.format(body))
+    print('\t- utf8 content: {}'.format(body.decode("utf-8")))
